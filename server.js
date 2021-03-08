bdcc7b082c56cbf3ddc6229f7961faca4608c2dd
veerleprins <veerleprins@hotmail.com>
2021-03-08 14:47:36 +0100

    Downloaded template engine ejs & added views files

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // Externals
 const express = require("express");
 const router = express();
+require("dotenv").config();
 
 // Internals
-const PORT = "3000";
+const PORT = process.env.PORT;
+
+// Middleware
+router.set("view engine", "ejs");
+router.set("views", "views");
 
 // Routes
-router.get("/", () => {
+router.get("/", (req, res) => {
   res.send(`This is a test`);
+});
+
+router.get("/*", (req, res) => {
+  res.status(404).render("404");
 });
 
 router.listen(PORT, () => {
