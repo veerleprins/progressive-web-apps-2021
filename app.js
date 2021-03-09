38373a3b24e728ecc9e9593af98f5de983272ebf
veerleprins <veerleprins@hotmail.com>
2021-03-09 09:16:40 +0100

    Created server file with routes

---
+++ app.js
@@ -0,0 +1,20 @@
+// Externals
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+// Routes
+const routes = require("./router/routes");
+
+// Internals
+const PORT = process.env.PORT;
+
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.static("static"));
+
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
