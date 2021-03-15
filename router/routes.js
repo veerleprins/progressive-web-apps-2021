9aa21bd6fa17630d1dd51d18c6eca6520fc103e3
veerleprins <veerleprins@hotmail.com>
2021-03-15 13:49:38 +0100

    Removed object format & added comments

--- router/routes.js
+++ router/routes.js
@@ -1,18 +1,20 @@
+// Externals
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Functions
-const { home } = require("./renders/home");
-const { movie } = require("./renders/movie");
-const { search } = require("./renders/search");
-const { error } = require("./renders/error");
+const home = require("./renders/home");
+const movie = require("./renders/movie");
+const search = require("./renders/search");
+const error = require("./renders/error");
 
 // Routes
-router.get("/", home);
-router.get("/:id", movie);
-router.post("/", urlencodedParser, search);
-router.get("/*", error);
+router
+  .get("/", home)
+  .get("/movies/:id", movie)
+  .post("/", urlencodedParser, search)
+  .get("/*", error);
 
 module.exports = router;
