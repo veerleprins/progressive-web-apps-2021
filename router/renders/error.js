ff58607afad8cdde5088eec504c1e76d0330d4c0
veerleprins <veerleprins@hotmail.com>
2021-03-16 15:31:14 +0100

    Added base & removed urls within server

--- router/renders/error.js
+++ router/renders/error.js
@@ -1,6 +1,8 @@
 const error = (req, res) => {
   try {
-    res.status(404).render("404", { pageTitle: "Page not found", styleLink: "/styles.main.css" });
+    res.status(404).render("404", {
+      pageTitle: "Page not found"
+    });
   } catch (err) {
     console.log(err);
   }
