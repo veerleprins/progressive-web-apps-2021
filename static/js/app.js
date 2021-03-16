5ca23726edf5ad6cc9f66ba22adf02fcd00f4a27
veerleprins <veerleprins@hotmail.com>
2021-03-16 15:35:43 +0100

    Added caching & removing old caching

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-// Check if service workers are supported
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((reg) => console.log(`Service Worker: Registered on: ${reg.scope}`))
-      .catch((err) => console.log(`Service Worker: Error: ${err}`));
-  });
-}
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,39 @@
+const CACHE_NAME = "cache-v1";
+
+const CACHE_ASSETS = [
+  "/",
+  "/offline/",
+  "./styles/main.css",
+  "./scripts/app.js",
+];
+
 // Call Install Event:
 self.addEventListener("install", (event) => {
   console.log("Service Worker: Installed");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: Caching Files");
+      return cache.addAll(CACHE_ASSETS).then(() => self.skipWaiting());
+    })
+  );
 });
 
 // Call Activate Event:
 self.addEventListener("activate", (event) => {
   console.log("Service Worker: Activated");
+
+  // Remove Unwanted Caches
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
